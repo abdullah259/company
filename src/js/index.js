9eed5054a70f2b430e0cb5be6f5f613e1c1dd467
AbdullahGhazi <abdullahghazi404@gmail.com>
2021-04-03 17:25:00 -0700

    main page

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+import '@laylazi/bootstrap-rtl/dist/js/bootstrap.min.js';
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/popper.min.js';
+import '../sass/style.scss';
+import '@fortawesome/fontawesome-free/js/all.min.js'
+
+
+$(function () {
+    $('.thumbnail2').hover(function() {
+        $(this).find('.project-category').hide();
+        $(this).find('.caption2').slideDown(250);
+    },
+    function() {
+        $(this).find('.caption2').slideUp(250);
+        $(this).find('.project-category').show();
+    }
+    );
+});
+
+var date = new Date();
+var year = date.getFullYear();
+document.getElementById("date").innerHTML = year;
